6606ba1e77037d47f1e135057ab8dfb3f8f8054c
vhperaltaj <victor.peralta@microside.com>
2024-08-28 16:08:53 -0600

    Added proper format

--- Proyecto/PIC16F877A/DHT11.c
+++ Proyecto/PIC16F877A/DHT11.c
@@ -13,14 +13,14 @@ display LCD I2C.
 ---------------------------------------------------------------------------
 */
 
-#include <16F877A.h>               //Incluye el microcontrolador con el que se va a trabajar
-#use delay(clock = 20Mhz, crystal) // Tipo de oscilador y frecuencia dependiendo del microcontrolador
-#use i2c(master, sda = PIN_B0, scl = PIN_B1)
+#include <16F877A.h>                 //Incluye el microcontrolador con el que se va a trabajar
+#use delay( clock = 20Mhz, crystal ) // Tipo de oscilador y frecuencia dependiendo del microcontrolador
+#use i2c( master, sda = PIN_B0, scl = PIN_B1 )
 
 #define DHT11_PIN PIN_B2
 
-#include "flex_lcd.h" //LIBRERIA LCD
 #include "dht_sensor.h"
+#include "flex_lcd.h"                //LIBRERIA LCD
 
 float temperature = 0;
 float humidity = 0;
@@ -29,47 +29,43 @@ int dht11_stat = 0;
 void main()
 {
 
-    delay_ms(1000);
-    lcd_i2c_init(0x27); // Inicializa la pantalla LCD
+    delay_ms( 1000 );
+    lcd_i2c_init( 0x27 );            // Inicializa la pantalla LCD
 
     lcd_i2c_clear();
-    lcd_i2c_setCursor(3, 0);
-    lcd_i2c_write_byte("DHT11");
-    lcd_i2c_setCursor(3, 1);
+    lcd_i2c_setCursor( 3, 0 );
+    lcd_i2c_write_byte( "DHT11" );
+    lcd_i2c_setCursor( 3, 1 );
 
-    if (dht11_init() != 0)
-    { // Inicializa el DHT11
-        lcd_i2c_write_byte("ERROR");
-        while (1)
-        {
+    if ( dht11_init() != 0 ) {       // Inicializa el DHT11
+        lcd_i2c_write_byte( "ERROR" );
+        while ( 1 ) {
         }
     }
 
-    lcd_i2c_write_byte("OK");
-    delay_ms(1000);
+    lcd_i2c_write_byte( "OK" );
+    delay_ms( 1000 );
 
-    while (1)
-    {
-        delay_ms(2000);
+    while ( 1 ) {
+        delay_ms( 2000 );
         // Lee el sensor DHT11
-        dht11_stat = dht11_read(&temperature, &humidity);
+        dht11_stat = dht11_read( &temperature, &humidity );
         // Cualquier valor diferente a 0 significa error en la lectura
-        if (dht11_stat != 0)
-        {
+        if ( dht11_stat != 0 ) {
             lcd_i2c_clear();
-            lcd_i2c_setCursor(0, 0);
-            lcd_i2c_write_byte("DHT11 Error");
+            lcd_i2c_setCursor( 0, 0 );
+            lcd_i2c_write_byte( "DHT11 Error" );
             continue;
         }
 
         lcd_i2c_clear();
-        lcd_i2c_setCursor(0, 0);
-        lcd_i2c_write_byte("T: ");
-        lcd_i2c_write_double(temperature, 2);
-        lcd_i2c_write_byte(" C");
-        lcd_i2c_setCursor(0, 1);
-        lcd_i2c_write_byte("H: ");
-        lcd_i2c_write_double(humidity, 2);
-        lcd_i2c_write_byte(" %");
+        lcd_i2c_setCursor( 0, 0 );
+        lcd_i2c_write_byte( "T: " );
+        lcd_i2c_write_double( temperature, 2 );
+        lcd_i2c_write_byte( " C" );
+        lcd_i2c_setCursor( 0, 1 );
+        lcd_i2c_write_byte( "H: " );
+        lcd_i2c_write_double( humidity, 2 );
+        lcd_i2c_write_byte( " %" );
     }
 }
